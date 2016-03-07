862c51c7cef96157f7042b73c6c1c0465d145449
Johan Protin <jprotin@hipay.com>
2016-03-07 11:53:10 +0100

    Delete in index hipay_tpp

--- hipay_tpp/hipay_tpp.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (_PS_VERSION_ < '1.5') {
-	// version 1.4
-	require_once(_PS_ROOT_DIR_.'/modules/hipay_tpp/1.4/hipay_tpp.php');
-} else {
-	// Version 1.5 or above
-	require_once(_PS_ROOT_DIR_.'/modules/hipay_tpp/1.5/hipay_tpp.php');
-}
--- hipay_tpp/validation_process.php
+++
@@ -1,7 +0,0 @@
-<?php
-include(dirname(__FILE__).'/../../config/config.inc.php');
-
-if (_PS_VERSION_ < '1.5') {
-	// version 1.4
-	include (dirname ( __FILE__ ) . '/1.4/validation_process.php');
-} 
