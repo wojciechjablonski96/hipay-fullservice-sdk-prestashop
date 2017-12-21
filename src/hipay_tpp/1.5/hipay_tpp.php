c0d4337d831e078ad746093d45271d82abc2b812
Johan PROTIN <jprotin@hipay.com>
2017-12-21 17:25:23 +0100

    hotfix 1.3.16

--- src/hipay_tpp/1.5/hipay_tpp.php
+++ src/hipay_tpp/1.5/hipay_tpp.php
@@ -38,7 +38,7 @@ class HiPay_Tpp extends PaymentModule {
 	public function __construct() {
 		$this->name = 'hipay_tpp';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.3.15';
+		$this->version = '1.3.16';
 		$this->module_key = 'e25bc8f4f9296ef084abf448bca4808a';
 		$this->author = 'HiPay';
 
